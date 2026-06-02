cd67c9a5dddfc710f205a445e6efbda92cf1cebe
Waiz Wafiq <mwaizwafiq@gmail.com>
2026-06-02 08:19:37 +0800

    feat(doctor): add db-privileges probe rejecting superuser and all-privileges

--- nengok/diagnostics/__init__.py
+++ nengok/diagnostics/__init__.py
@@ -14,6 +14,7 @@ from nengok.diagnostics.agent_runner_probe import probe_agent_runner
 from nengok.diagnostics.base import Probe, ProbeResult, ProbeStatus
 from nengok.diagnostics.baseline_prompt_probe import probe_baseline_prompt
 from nengok.diagnostics.config_probe import probe_config_file
+from nengok.diagnostics.db_privileges import probe_db_privileges
 from nengok.diagnostics.gemini_probe import probe_gemini
 from nengok.diagnostics.phoenix_probe import probe_phoenix
 from nengok.diagnostics.phoenix_project_probe import probe_phoenix_project
@@ -23,6 +24,7 @@ DEFAULT_PROBES: tuple[Probe, ...] = (
     probe_phoenix,
     probe_phoenix_project,
     probe_gemini,
+    probe_db_privileges,
     probe_baseline_prompt,
     probe_agent_runner,
 )
@@ -35,6 +37,7 @@ __all__ = [
     "probe_agent_runner",
     "probe_baseline_prompt",
     "probe_config_file",
+    "probe_db_privileges",
     "probe_gemini",
     "probe_phoenix",
     "probe_phoenix_project",
